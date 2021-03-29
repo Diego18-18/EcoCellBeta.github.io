0c69a257a8203d6c0b98901e3f57243055ab5a49
Diego <diegoprogramador18@gmail.com>
2021-03-29 11:23:18 -0300

    Guardado 1

---
+++ Inicio.js
@@ -0,0 +1,9 @@
+document.querySelector('.menu-btn').addEventListener('click', () => {
+    document.querySelector('.categorías').classList.toggle('up');
+});
+
+ScrollReveal().reveal('.bienvenida');
+ScrollReveal().reveal('.grupo1');
+ScrollReveal().reveal('.grupo2');
+ScrollReveal().reveal('.grupo3');
+ScrollReveal().reveal('.grupo4');
